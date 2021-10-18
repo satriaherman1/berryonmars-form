4dbe646b855ad94ee0a4075fe1afe5b3afb60b95
satriaherman <muhammadsatriaherman@gmail.com>
2021-10-18 18:58:39 +0700

    fix search list not show

--- assets/js/component/select/custom-select-with-search.js
+++ assets/js/component/select/custom-select-with-search.js
@@ -3,11 +3,16 @@ const allSelectSearch = document.querySelectorAll('.custom-select-search')
 let selectIndex;
 const optionArr = []
 
-const optionListSearchClick = (opt, index, selectId = null) => {
+const optionListSearchClick = (opt, index, selectId = null, ulId = null) => {
     const select = document.querySelector(selectId)
+    const ul = document.querySelector(ulId)
+    console.log(ulId)
     if(selectId !== null){
         select.value = opt.textContent;
-        select.parentElement.classList.add('d-none')
+        // select.parentElement.classList.add('d-none');
+        if(ulId !== null){
+            ul.parentElement.classList.add('d-none')
+        }
     }
     else{
         const currentSelect = customSelectSearchField[index].querySelector('select');
@@ -16,14 +21,18 @@ const optionListSearchClick = (opt, index, selectId = null) => {
     opt.parentElement.classList.add('d-none');
 }
 
-const selectSearchClick = (select) => {
+const selectSearchClick = (select, toggleUl = false) => {
     const currentCustomSelectListBox = select.parentElement.querySelector('.custom-select-list-box')
+    if(toggleUl){
+        const ul = select.parentElement.querySelector('.custom-select-list');
+        ul.classList.remove('d-none')
+    }
     currentCustomSelectListBox.classList.toggle('d-none')
 }
 
 const search = (el) => {
     const select = el.parentElement.parentElement.parentElement.querySelector('select')
-    const ul = el.parentElement.parentElement.querySelector('ul.custom-select-list')
+    const ul = el.parentElement.parentElement.querySelector('.custom-select-list')
     let match = optionArr.filter(value => new RegExp(`${el.value}`, 'i').test(value))
 
     select.innerHTML =''
@@ -33,9 +42,9 @@ const search = (el) => {
             <option value="${item}">${item}</option>
         ` 
         ul.innerHTML += `<li class="custom-option-list" 
-                        onclick="optionListSearchClick(this , ${selectIndex}), '#${ul.id}'">${item}</li>`
+                        onclick="optionListSearchClick(this , ${selectIndex}, '#${select.id}', '#${ul.id}')">${item}</li>`
     })
-    console.log(match)
+  
 
 }
 
@@ -59,10 +68,21 @@ customSelectSearchField.forEach((field , index) => {
             optionArr.push(el.value)
             customSelectList.innerHTML += `<li class="custom-option-list" 
                                             onclick="optionListSearchClick(this , ${index})">${el.value}</li>`
-            console.log(customSelectList)
+           
         }
     })
 
 
 })
 
+// const renderList = (element) => {
+//     const opt = element.querySelector('option');
+//     opt.forEach((el) => {
+//         if(!el.disabled){
+//             optionArr.push(el.value)
+//             customSelectList.innerHTML += `<li class="custom-option-list" 
+//                                             onclick="optionListSearchClick(this , ${index})">${el.value}</li>`
+           
+//         }
+//     })
+// }
